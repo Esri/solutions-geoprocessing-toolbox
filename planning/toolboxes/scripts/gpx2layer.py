d5557496a1a798c2f0ac203fd855551e98b9afee
Ben Conklin <bconklin@esri.com>
2014-05-06 14:27:10 -0700

    Added Patrol Data Capture Toolbox
    Added the Patrol Data Capture Toolbox to the repository

---
+++ planning/toolboxes/scripts/gpx2layer.py
@@ -0,0 +1,142 @@
+#--------ESRI 2010-------------------------------------
+# GPX (from XML) to layer
+# This script will take a GPX file as
+# input and convert it to a featureclass
+# INPUTS: 
+#    GPX file (FILE)
+# OUTPUTS:
+#    Output Featureclass (FEATURECLASS)
+#    Line Featureclass  (FEATURECLASS) - Optional
+# 
+# Date: May 5, 2010
+# Modified: June 10, 2010
+#------------------------------------------------------
+
+from xml.etree import ElementTree
+from datetime import datetime
+import arcpy, string
+import re
+import sys, os
+
+#Set the workspace environments relative to the script location
+scriptPath = sys.path[0]
+toolSharePath = os.path.dirname(scriptPath)
+currentworkspace = os.path.join(toolSharePath, r"..\Data\Data.gdb")
+scratchworkspace = os.path.join(toolSharePath, r"..\Data\Scratch.gdb")
+try:
+    if not(arcpy.Exists(currentworkspace)):
+        arcpy.CreateFileGDB_management(currentworkspace)
+except Exception, ErrorDesc:
+    arcpy.AddError("Current workspace does not exist and it was not possible to create it\n" + str(ErrorDesc))
+try:
+    if not(arcpy.Exists(scratchworkspace)):
+        arcpy.CreateFileGDB_management(scratchworkspace)
+except Exception, ErrorDesc:
+    arcpy.AddError("Scratch workspace does not exist and it was not possible to create it\n" + str(ErrorDesc))
+
+arcpy.env.workspace = currentworkspace
+arcpy.env.scratchworkspace = scratchworkspace
+
+def trkpt2dict(gpxfile):
+    #Generator : for each trkpt return point + all other attributes as a dictionary
+   
+    TOPOGRAFIX_NS = './/{http://www.topografix.com/GPX/1/1}'        
+    TRACKPOINT_NS = TOPOGRAFIX_NS + 'extensions/{http://www.garmin.com/xmlschemas/TrackPointExtension/v1}TrackPointExtension/{http://www.garmin.com/xmlschemas/TrackPointExtension/v1}'
+    
+    tree = ElementTree.parse(gpxfile)
+       
+    for node in tree.findall(TOPOGRAFIX_NS + 'trkpt'):
+        d = {}
+        y = node.attrib.get('lat')
+        x = node.attrib.get('lon')
+        z = node.find(TOPOGRAFIX_NS + 'ele').text
+        ele = node.find(TOPOGRAFIX_NS + 'ele').text
+        t = node.find(TOPOGRAFIX_NS + 'time').text      
+        # __Extension Try Block__
+        # This section useful for extension elements within a given GPX file, for example "hr" (heartrate)
+        #try:
+        #    hr = node.find(TRACKPOINT_NS + 'hr').text
+        #except:
+        #    hr = 0
+        #     
+        #yield results from reading GPX.
+        #additional entries should be added to the end if included in above Try block
+        yield arcpy.Point(x,y,z), ele, t  #, hr
+        
+def parse_timestamp(s): 
+  #Returns (datetime, tz offset in minutes) or (None, None) 
+  m = re.match(""" ^ 
+    (?P<year>-?[0-9]{4}) - (?P<month>[0-9]{2}) - (?P<day>[0-9]{2}) 
+    T (?P<hour>[0-9]{2}) : (?P<minute>[0-9]{2}) : (?P<second>[0-9]{2}) 
+    (?P<microsecond>\.[0-9]{1,6})? 
+    (?P<tz> 
+      Z | (?P<tz_hr>[-+][0-9]{2}) : (?P<tz_min>[0-9]{2}) 
+    )? 
+    $ """, s, re.X) 
+  if m is not None: 
+    values = m.groupdict() 
+    if values["tz"] in ("Z", None): 
+      tz = 0 
+    else: 
+      tz = int(values["tz_hr"]) * 60 + int(values["tz_min"]) 
+    if values["microsecond"] is None: 
+      values["microsecond"] = 0 
+    else: 
+      values["microsecond"] = values["microsecond"][1:] 
+      values["microsecond"] += "0" * (6 - len(values["microsecond"])) 
+    values = dict((k, int(v)) for k, v in values.iteritems() 
+                  if not k.startswith("tz")) 
+    try: 
+      return datetime(**values), tz 
+    except ValueError: 
+      pass 
+  return None, None 
+
+if __name__ == "__main__":
+    
+    #Get GPX and Output directory paramaters
+    gpxfile = arcpy.GetParameterAsText(0)        
+    outFC = arcpy.GetParameterAsText(1)
+    
+    outPath = outFC[0:outFC.rfind("\\")]
+    outName = outFC[outFC.rfind("\\")+1:len(outFC)]
+   
+      
+    try:       
+       
+        #Create the FC, add appropriate fields
+        arcpy.CreateFeatureclass_management(outPath, outName, "POINT", "", "", "ENABLED",4326)
+        arcpy.AddField_management(outFC, "Date_Time", "DATE")
+        arcpy.AddField_management(outFC, "Elevation", "DOUBLE")
+        #arcpy.AddField_management(outFC, "hr", "TEXT")   #This is an extension field
+        #For every extension field added in the try block, insert an appropriate AddField
+                      
+    except Exception, ErrorDesc:
+        arcpy.AddError(str(ErrorDesc))
+        
+    
+    arcpy.env.workspace = outPath
+        
+    rows = arcpy.InsertCursor(outFC)
+
+    recComplete = 0    
+    
+    # walk through each trkpt, create and insert a record into the feature class for each
+    for pt, ele, t in trkpt2dict(gpxfile):   # Add in each value within the Extension Try block         
+        row = rows.newRow()           
+        row.SHAPE = pt
+        row.Elevation = ele
+        row.Date_Time = parse_timestamp(t)[0]
+        #row.hr = hr   #Extension element
+        #Add an entry for each additional entry in the Extension Try block within the dictionary
+        rows.insertRow(row)
+        recComplete += 1
+        
+        if (recComplete % 2000) == 0:            
+            arcpy.AddMessage("Processed " + str(recComplete) + " records.")
+        
+   
+    #If "Point to Line" is required, create second output of line featureclass
+    if arcpy.GetParameter(2) == 1:
+        arcpy.PointsToLine_management(outFC, outFC + "_line")                                
+        arcpy.SetParameterAsText(3, outFC + "_line")
