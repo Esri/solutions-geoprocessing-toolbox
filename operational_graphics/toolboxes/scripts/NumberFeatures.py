4e561c8cf19dd0f1c392b13719b6e45a88ba0020
Janette Scott <jscott@esri.com>
2015-02-19 11:08:41 -0500

    Fixes and updates to the Number Features tool.

--- operational_graphics/toolboxes/scripts/NumberFeatures.py
+++ operational_graphics/toolboxes/scripts/NumberFeatures.py
@@ -65,73 +65,93 @@ def findLayerByName(layerName, gisVersion):
 pointFeatureName = os.path.basename(pointFeatures)
 layerExists = False
 
-gisVersion = arcpy.GetInstallInfo()["Version"]
+try:
+    # Check that area to number is a polygon
+    descArea = arcpy.Describe(areaToNumber)
+    areaGeom = descArea.shapeType
+    arcpy.AddMessage("Shape type: " + str(areaGeom))
+    if (descArea.shapeType != "Polygon"):
+        raise Exception("ERROR: The area to number must be a polygon.")
 
-mxd, df, aprx, mp = None, None, None, None
-if gisVersion == "1.0": #Pro:
-    from arcpy import mp
-    aprx = arcpy.mp.ArcGISProject("CURRENT")
-    maplist = aprx.listMaps()[0]
-    for lyr in maplist.listLayers():
-        if lyr.name == pointFeatureName:
-            layerExists = True
-else:
-    from arcpy import mapping
-    mxd = arcpy.mapping.MapDocument('CURRENT')
-    for lyr in arcpy.mapping.ListLayers(mxd):
-        if lyr.name == pointFeatureName:
-            layerExists = True
+    gisVersion = arcpy.GetInstallInfo()["Version"]
 
-if layerExists == False:
-    arcpy.MakeFeatureLayer_management(pointFeatures, pointFeatureName)
-else:
-    pointFeatureName = pointFeatures
+    mxd, df, aprx, mp = None, None, None, None
+    if gisVersion == "1.0": #Pro:
+        from arcpy import mp
+        aprx = arcpy.mp.ArcGISProject("CURRENT")
+        maplist = aprx.listMaps()[0]
+        for lyr in maplist.listLayers():
+            if lyr.name == pointFeatureName:
+                layerExists = True
+    else:
+        from arcpy import mapping
+        mxd = arcpy.mapping.MapDocument('CURRENT')
+        for lyr in arcpy.mapping.ListLayers(mxd):
+            if lyr.name == pointFeatureName:
+                layerExists = True
 
-# Select all the points that are inside of area
-arcpy.AddMessage("Selecting points inside of the area")
-selectionLayer = arcpy.SelectLayerByLocation_management(pointFeatureName, "intersect", areaToNumber)
+    if layerExists == False:
+        arcpy.MakeFeatureLayer_management(pointFeatures, pointFeatureName)
+    else:
+        pointFeatureName = pointFeatures
 
-# If no output FC is specified, then set it a temporary one, as this will be copied to the input and then deleted.
-# Sort layer by upper right across and then down spatially,
-overwriteFC = False;
-if (outputFeatureClass == ""):
-    outputFeatureClass = "tempSortedPoints"
-    overwriteFC = True;
-arcpy.AddMessage("Sorting the selected points geographically, right to left, top to bottom")
-arcpy.Sort_management(selectionLayer, outputFeatureClass, [["Shape", "ASCENDING"]])
+    # Select all the points that are inside of area
+    arcpy.AddMessage("Selecting points inside of the area")
+    selectionLayer = arcpy.SelectLayerByLocation_management(pointFeatureName, "intersect", areaToNumber)
 
-# Number the fields
-arcpy.AddMessage("Numbering the fields")
-i = 1
-cursor = arcpy.UpdateCursor(outputFeatureClass)
-for row in cursor:
-    row.setValue(numberingField, i)
-    cursor.updateRow(row)
-    i += 1
+    # If no output FC is specified, then set it a temporary one, as this will be copied to the input and then deleted.
+    # Sort layer by upper right across and then down spatially,
+    overwriteFC = False;
+    if (outputFeatureClass == ""):
+        outputFeatureClass = "tempSortedPoints"
+        overwriteFC = True;
+    arcpy.AddMessage("Sorting the selected points geographically, right to left, top to bottom")
+    arcpy.Sort_management(selectionLayer, outputFeatureClass, [["Shape", "ASCENDING"]])
 
-# Clear the selection
-arcpy.AddMessage("Clearing the selection")
-arcpy.SelectLayerByAttribute_management(pointFeatureName, "CLEAR_SELECTION")
+    # Number the fields
+    arcpy.AddMessage("Numbering the fields")
+    i = 1
+    cursor = arcpy.UpdateCursor(outputFeatureClass)
+    for row in cursor:
+        row.setValue(numberingField, i)
+        cursor.updateRow(row)
+        i += 1
 
-# Overwrite the Input Point Features, and then delete the temporary output feature class
-targetLayerName = ""
-if (overwriteFC):
-    arcpy.AddMessage("Copying the features to the input, and then deleting the temporary feature class")
-    desc = arcpy.Describe(pointFeatureName)
-    arcpy.CopyFeatures_management(outputFeatureClass, desc.path + "\\" + pointFeatureName)
-    arcpy.Delete_management(outputFeatureClass)
-    #UPDATE
-    #if layerExists == False:
-        #layerToAdd = arcpy.mapping.Layer(pointFeatureName)
+    # Clear the selection
+    arcpy.AddMessage("Clearing the selection")
+    arcpy.SelectLayerByAttribute_management(pointFeatureName, "CLEAR_SELECTION")
+
+    # Overwrite the Input Point Features, and then delete the temporary output feature class
+    targetLayerName = ""
+    if (overwriteFC):
+        arcpy.AddMessage("Copying the features to the input, and then deleting the temporary feature class")
+        desc = arcpy.Describe(pointFeatureName)
+        overwriteFC = os.path.join(os.path.sep, desc.path, pointFeatureName)
+        fields = (numberingField, "SHAPE@")
+        overwriteCursor = arcpy.da.UpdateCursor(overwriteFC, fields)
+        for overwriteRow in overwriteCursor:
+            sortedPointsCursor = arcpy.da.SearchCursor(outputFeatureClass, fields)
+            for sortedRow in sortedPointsCursor:
+                if sortedRow[1].equals(overwriteRow[1]):
+                    overwriteRow[0] = sortedRow[0]
+            overwriteCursor.updateRow(overwriteRow)
+        arcpy.Delete_management(outputFeatureClass)
+
+        #UPDATE
+        #if layerExists == False:
+            #layerToAdd = arcpy.mapping.Layer(pointFeatureName)
+            #arcpy.mapping.AddLayer(df, layerToAdd, "AUTO_ARRANGE")
+        targetLayerName = pointFeatureName
+    else:
+        #UPDATE
+        #layerToAdd = arcpy.mapping.Layer(outputFeatureClass)
         #arcpy.mapping.AddLayer(df, layerToAdd, "AUTO_ARRANGE")
-    targetLayerName = pointFeatureName
-else:
-    #UPDATE
-    #layerToAdd = arcpy.mapping.Layer(outputFeatureClass)
-    #arcpy.mapping.AddLayer(df, layerToAdd, "AUTO_ARRANGE")
-    targetLayerName = os.path.basename(outputFeatureClass)
+        targetLayerName = os.path.basename(outputFeatureClass)
 
-# Get and label the output feature
-layer = findLayerByName(targetLayerName, gisVersion)
-if(layer):
-    labelFeatures(layer, numberingField)
+    # Get and label the output feature
+    layer = findLayerByName(targetLayerName, gisVersion)
+    if(layer):
+        labelFeatures(layer, numberingField)
+
+except Exception, ex:
+    arcpy.AddError(ex)
