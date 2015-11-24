2af22f332a8966b34733922056cf03235a5c1ecc
JudTown17 <judson.hardy@gmail.com>
2015-11-23 16:34:00 -0800

    Parameterized log file name from TestKickStart.bat

--- utils/test/TestKickStart.bat
+++ utils/test/TestKickStart.bat
@@ -29,16 +29,21 @@ rem ==================================================
 rem  history:
 rem  10/06/2015 - MF - placeholder
 rem  10/30/2015 - MF - tests running
+rem 11/23/2015 - JH - added paremeter for default log file name
 rem ==================================================
+
+REM usage: set LOG=<defaultLogFileName.log>
+REM name is optional; if not specified, name will be specified for you
+set LOG=default.log
 
 REM =====================================================
 REM If you have BOTH versions of Python installed:
 
 ECHO Python 3.4 Tests ===============================
-py -3.4 TestRunner.py
+py -3.4 TestRunner.py %LOG%
 
 ECHO Python 2.7 Tests ===============================
-py -2.7 TestRunner.py
+py -2.7 TestRunner.py %LOG%
 
 REM =====================================================
 REM If you only have ONE version of Python installed:
