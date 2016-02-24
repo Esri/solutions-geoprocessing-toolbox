b26e43485babc820aec30def2d4cbbd2902c50ca
JudTown17 <judson.hardy@gmail.com>
2016-02-23 16:57:07 -0800

    Added Import WMO Stations test

--- utils/test/suitability_tests/ImportWMOStationsTestCase.py
+++ utils/test/suitability_tests/ImportWMOStationsTestCase.py
@@ -40,24 +40,50 @@ class ImportWMOStationsTestCase(unittest.TestCase):
     ''' Test all tools and methods related to the Import WMO Station Data tool
     in the Military Aspects of Weather toolbox'''
     
+    WMOGDB = None
+    WMOStationsFC = None
+    stationsTextFile = None
+    
     def setUp(self):
         if Configuration.DEBUG == True: print("     ImportWMOStationsTestCase.setUp")
         UnitTestUtilities.checkArcPy()
         Configuration.suitabilityDataPath = os.path.join(Configuration.suitabilityPaths, "data")
+        self.stationsTextFile = os.path.join(Configuration.suitabilityDataPath, "WMOStations.tab")
+        self.WMOGDB = os.path.join(Configuration.suitabilityDataPath, "WMO.gdb")
+        self.WMOStationsFC = os.path.join(self.WMOGDB, "WMOFC_output")
         if (Configuration.suitabilityScratchGDB == None) or (not arcpy.Exists(Configuration.suitabilityScratchGDB)):
             Configuration.suitabilityScratchGDB = UnitTestUtilities.createScratch(Configuration.suitabilityDataPath)
-        UnitTestUtilities.checkFilePaths([Configuration.suitabilityDataPath, Configuration.maow_ToolboxPath])
-        # scratch GDB needed for output
+        UnitTestUtilities.checkFilePaths([Configuration.suitabilityDataPath, Configuration.maow_ToolboxPath, self.WMOGDB])
+        if (arcpy.Exists(self.WMOStationsFC)):
+            arcpy.Delete_management(self.WMOStationsFC)
+            arcpy.AddMessage("Deleted previous output feature class: " + self.WMOStationsFC)
     
     def tearDown(self):
         if Configuration.DEBUG == True: print("     ImportWMOStationsTestCase.tearDown")
         UnitTestUtilities.deleteScratch(Configuration.suitabilityScratchGDB)
         
     def test_import_wmo_stations(self):
-        if Configuration.DEBUG == True: print("     ImportWMOStationsTestCase.test_import_wmo_stations")
-        arcpy.AddMessage("Testing Import WMO Stations (Desktop)")
+        try:
+            if Configuration.DEBUG == True: print("     ImportWMOStationsTestCase.test_import_wmo_stations")
+            arcpy.AddMessage("Testing Import WMO Stations (Desktop)")
         
-        runToolMessage = "Running tool (Import WMO Stations)"
-        arcpy.AddMessage(runToolMessage)
-        Configuration.Logger.info(runToolMessage)
+            arcpy.ImportToolbox(Configuration.maow_ToolboxPath, "maow")
+            runToolMessage = "Running tool (Import WMO Stations)"
+            arcpy.AddMessage(runToolMessage)
+            Configuration.Logger.info(runToolMessage)
+            
+            arcpy.ImportWMOStations_maow(self.stationsTextFile, self.WMOStationsFC)
+            
+            # 12718 features should be in output feature class
+            wmoStationCount = int(arcpy.GetCount_management(self.WMOStationsFC).getOutput(0))
+            self.assertEqual(wmoStationCount, int(12718))
+            
+        except arcpy.ExecuteError:
+            UnitTestUtilities.handleArcPyError()
+            
+        except:
+            UnitTestUtilities.handleGeneralError()
+        
+        
+        
             
