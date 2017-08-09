9a0a72de673a023a002ebf3e2b7b40591d133221
Chris Moore <csmooreacm@acm.org>
2017-08-09 17:09:51 -0400

    Readd Clearing Ops tests in clean branch

---
+++ utils/test/clearing_operations_tests/ClearingOperationsTestSuite.py
@@ -0,0 +1,43 @@
+#------------------------------------------------------------------------------
+# Copyright 2017 Esri
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#------------------------------------------------------------------------------
+
+import unittest
+import logging
+import Configuration
+
+from . import ClearingOperationsPointTargetTestCase
+from . import ClearingOperationsCanvasAreaGRGTestCase
+from . import ClearingOperationsNumberFeaturesTestCase
+
+''' Test suite for all tools in the Clearing Operationss Tools toolbox '''
+
+def getTestSuite():
+
+    if Configuration.DEBUG == True:
+        print("      ClearingOperationsTestSuite.getSuite")
+
+    testSuite = unittest.TestSuite()
+
+    ''' Add the Clearing Operations tests '''
+
+    loader = unittest.TestLoader()
+
+
+    testSuite.addTest(loader.loadTestsFromTestCase(ClearingOperationsCanvasAreaGRGTestCase.ClearingOperationsCanvasAreaGRGTestCase))
+    testSuite.addTest(loader.loadTestsFromTestCase(ClearingOperationsNumberFeaturesTestCase.ClearingOperationsNumberFeaturesTestCase))
+    testSuite.addTest(loader.loadTestsFromTestCase(ClearingOperationsPointTargetTestCase.ClearingOperationsPointTargetTestCase))
+
+
+    return testSuite
