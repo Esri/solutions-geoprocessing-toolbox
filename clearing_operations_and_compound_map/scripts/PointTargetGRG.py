243ed697eb24195bbe9c47ab58715cd650f926f9
Matt Funk <mfunk@esri.com>
2017-08-09 07:47:39 -0700

    Fixed tab/space mess in line 581 PointTargetGRG.py

--- clearing_operations_and_compound_map/scripts/PointTargetGRG.py
+++ clearing_operations_and_compound_map/scripts/PointTargetGRG.py
@@ -578,7 +578,7 @@ def main():
             #mapLyr.labelClasses = labelLyr.labelClasses
             # turn labels on
             #mapLyr.showLabels = True
-			arcpy.AddMessage("Non-map environment, skipping labeling based on best practices")
+            arcpy.AddMessage("Non-map environment, skipping labeling based on best practices")
         else:
             arcpy.AddMessage("Non-map environment, skipping labeling...")
 
