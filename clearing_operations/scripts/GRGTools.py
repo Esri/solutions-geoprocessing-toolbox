cfd8a5e2be718d261a0387153be6001f08f143b3
Matt Funk <mfunk@esri.com>
2017-09-01 15:45:17 -0700

    Initial framework set up

---
+++ clearing_operations/scripts/GRGTools.py
@@ -0,0 +1,181 @@
+# coding: utf-8
+'''
+------------------------------------------------------------------------------
+ Copyright 2017 Esri
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+------------------------------------------------------------------------------
+ ==================================================
+ GRG.py
+ --------------------------------------------------
+ requirements: ArcGIS X.X, Python 2.7 or Python 3.x
+ author: ArcGIS Solutions
+ contact: support@esri.com
+ company: Esri
+ ==================================================
+ description: <Description>
+ ==================================================
+ history:
+ <date> - <initals> - <modifications>
+ ==================================================
+'''
+
+
+import os
+import sys
+import traceback
+import arcpy
+from arcpy import env
+import GRGUtilities
+
+class CanvasAreaGRG(object):
+    '''
+    '''
+    def __init__(self):
+        '''
+        '''
+        self.label = "Canvas Area GRG"
+        self.description = "Create a Gridded Reference Graphic (GRG) from an selected area on the map."
+
+    def getParameterInfo(self):
+        '''
+        Define parameter definitions
+        '''
+        input_area_features = arcpy.Parameter(name='input_canvas_area',
+                                              displayName='Input Canvas Area',
+                                              direction='Input',
+                                              datatype='GPFeatureRecordSetLayer',
+                                              parameterType='Required',
+                                              enabled=True,
+                                              multiValue=False)
+
+        fc = arcpy.CreateFeatureclass_management("in_memory",
+                                                "temp_poly",
+                                                "POLYGON",
+                                                "#",
+                                                "#",
+                                                "#",
+                                                arcpy.SpatialReference(54032)).getOutput(0)
+        input_area_features.value = arcpy.FeatureSet().load(fc)
+
+        cell_width = arcpy.Parameter(name='cell_width',
+                                     displayName='Cell Width',
+                                     direction='Input',
+                                     datatype='GPDouble',
+                                     parameterType='Required',
+                                     enabled=True,
+                                     multiValue=False)
+        cell_width.value = 100.0
+
+        cell_height = arcpy.Parameter(name='cell_height',
+                                      displayName='Cell Height',
+                                      direction='Input',
+                                      datatype='GPDouble',
+                                      parameterType='Required',
+                                      enabled=True,
+                                      multiValue=False)
+        cell_height.value = 100.0
+
+        cell_units = arcpy.Parameter(name='cell_units',
+                                     displayName='Cell Units',
+                                     direction='Input',
+                                     datatype='GPString',
+                                     parameterType='Required',
+                                     enabled=True,
+                                     multiValue=False)
+        cell_units.filter.type = 'ValueList'
+        cell_units.filter.list = ['METERS', 'FEET']
+        cell_units.value = cell_units.filter.list[0]
+
+        label_start_position = arcpy.Parameter(name='label_start_position',
+                                               displayName='Label Start Position',
+                                               direction='Input',
+                                               datatype='GPString',
+                                               parameterType='Required',
+                                               enabled=True,
+                                               multiValue=False)
+        label_start_position.filter.type = 'ValueList'
+        label_start_position.filter.list = ['UPPER_RIGHT',
+                                            'LOWER_RIGHT',
+                                            'UPPER_LEFT',
+                                            'LOWER_LEFT']
+        label_start_position.value = label_start_position.filter.list[0]
+
+        label_style = arcpy.Parameter(name='label_style',
+                                      displayName='Label Style',
+                                      direction='Input',
+                                      datatype='GPString',
+                                      parameterType='Required',
+                                      enabled=True,
+                                      multiValue=False)
+        label_style.filter.type = 'ValueList'
+        label_style.filter.list = ['ALPHA-ALPHA',
+                                   'ALPHA-NUMERIC',
+                                   'NUMERIC-NUMERIC']
+        label_style.value = label_style.filter.list[0]
+
+        output_features= arcpy.Parameter(name='output_grg-features',
+                                         displayName='Output GRG Features',
+                                         direction='Output',
+                                         datatype='DEFeatureClass',
+                                         parameterType='Required',
+                                         enabled=True,
+                                         multiValue=False)
+        output_features.value = r"%scratchGDB%/canvasgrg"
+
+        return [input_area_features,
+                cell_width,
+                cell_height,
+                cell_units,
+                label_start_position,
+                label_style,
+                output_features]
+
+    def updateParameters(self, parameters):
+        '''
+        Modify the values and properties of parameters before internal
+        validation is performed.  This method is called whenever a parameter
+        has been changed.
+        '''
+        return
+
+    def updateMessages(self, parameters):
+        '''
+        '''
+        return
+
+    def execute(self, parameters, messages):
+        ''' execute for toolbox'''
+        #arcpy.AddError("Not built yet.")
+
+        out_grg = GRGUtilities.GRGFromArea(AOI=parameters[0],
+                                           cellWidth=parameters[1],
+                                           cellHeight=parameters[2],
+                                           cellUnits=parameters[3],
+                                           labelStartPos=parameters[4],
+                                           labelStyle=parameters[5],
+                                           outputFeatureClass=parameters[6])
+        return out_grg
+
+class PointTargetGRG(object):
+    '''
+    '''
+    def __init__(self):
+        ''' Point Target GRG constructor '''
+        self.label = "Point Target GRG"
+        self.description = "Create a Gridded Reference Graphic (GRG) from an selected location on the map."
+
+class NumberFeatures(object):
+    '''
+    '''
+    def __init__(self):
+        ''' Number Features constructor '''
+        self.label = "Number Features"
+        self.description = "Number input point features within a selected area."
---
+++ clearing_operations/scripts/GRGUtilities.py
@@ -0,0 +1,377 @@
+# coding: utf-8
+'''
+------------------------------------------------------------------------------
+ Copyright 2017 Esri
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+------------------------------------------------------------------------------
+ ==================================================
+ GRGUtilities.py
+ --------------------------------------------------
+ requirements: ArcGIS X.X, Python 2.7 or Python 3.x
+ author: ArcGIS Solutions
+ contact: support@esri.com
+ company: Esri
+ ==================================================
+ description: <Description>
+ ==================================================
+ history:
+ <date> - <initals> - <modifications>
+ ==================================================
+'''
+
+import arcpy
+import Utilities
+
+import os, sys, math, traceback, inspect
+import arcpy
+from arcpy import env
+import Utilities
+
+
+DEBUG = True
+
+def labelFeatures(layer, field):
+    ''' set up labeling for layer '''
+    if appEnvironment == "ARCGIS_PRO":
+        if layer.supports("SHOWLABELS"):
+            for lblclass in layer.listLabelClasses():
+                lblclass.visible = True
+                lblclass.expression = " [" + str(field) + "]"
+            layer.showLabels = True
+    elif appEnvironment == "ARCMAP":
+        if layer.supports("LABELCLASSES"):
+            for lblclass in layer.labelClasses:
+                lblclass.showClassLabels = True
+                lblclass.expression = " [" + str(field) + "]"
+            layer.showLabels = True
+            arcpy.RefreshActiveView()
+    else:
+        pass # if returns "OTHER"
+
+def findLayerByName(layerName):
+    '''  '''
+    global mapList
+    global mxd
+    #UPDATE
+    #if isPro: #Update for automated test
+    if appEnvironment == "ARCGIS_PRO":
+        for layer in mapList.listLayers():
+            if layer.name == layerName:
+                arcpy.AddMessage("Found matching layer [" + layer.name + "]")
+                return layer
+            else:
+                arcpy.AddMessage("Incorrect layer: [" + layer.name + "]")
+    #else: #Update for automated test
+    elif appEnvironment == "ARCMAP":
+        for layer in arcpy.mapping.ListLayers(mxd):
+            if layer.name == layerName:
+                arcpy.AddMessage("Found matching layer [" + layer.name + "]")
+                return layer
+            else:
+                arcpy.AddMessage("Incorrect layer: [" + layer.name + "]")
+    else:
+        arcpy.AddWarning("Non-map environment")
+
+def ColIdxToXlName(index):
+    ''' Converts an index into a letter, labeled like excel columns, A to Z, AA to ZZ, etc.'''
+    ordA = ord('A')
+    ordZ = ord('Z')
+    len = ordZ - ordA + 1          
+    s = ""
+    while(int(index) >= 0):
+        s = chr(int(index) % len + ordA) + s
+        index = math.floor(int(index) / len) - 1
+    return s
+
+def GRGFromArea(AOI,
+                cellWidth,
+                cellHeight,
+                cellUnits,
+                labelStartPos,
+                labelStyle,
+                outputFeatureClass):
+    ''' Create GRG from an Area'''
+    try:
+        #UPDATE
+        gisVersion = arcpy.GetInstallInfo()["Version"]
+        appEnvironment = Utilities.GetApplication()
+        if DEBUG == True: arcpy.AddMessage("App environment: " + appEnvironment)
+        isPro = False
+        fc = os.path.join("in_memory", "AOI")
+        arcpy.CopyFeatures_management(AOI, fc)
+
+        #if gisVersion == "1.0": #Pro: #Update for automated test
+        if appEnvironment == "ARCGIS_PRO":
+            from arcpy import mp
+            aprx = arcpy.mp.ArcGISProject("CURRENT")
+            mapList = aprx.listMaps()[0]
+            isPro = True
+        #else: #Update for automated test
+        if appEnvironment == "ARCMAP":
+            from arcpy import mapping
+            mxd = arcpy.mapping.MapDocument('CURRENT')
+            df = arcpy.mapping.ListDataFrames(mxd)[0]
+            isPro = False
+
+        # From the template extent, create a polygon that we can project into a localized World Azimuthal Equidistan
+        if DEBUG == True: arcpy.AddMessage("Getting extent info...")
+
+        '''
+        ' If cell units are feet convert to meters
+        '''
+        if (cellUnits == "Feet"):
+            cellWidth = float(cellWidth) / 3.2808
+            cellHeight = float(cellHeight) / 3.2808
+
+        '''
+        ' create a minimum bounding rectangle around the AOI
+        ' The use of the MBG_FIELDS option in MinimumBoundingGeometry_management 
+        ' tool also creates a field that has the shape orientation
+        '''
+        arcpy.AddMessage("Getting Minimum Bounding Geometry that fits the Area of Interest")
+        minBound = os.path.join("in_memory","minBound")
+        arcpy.MinimumBoundingGeometry_management(fc, minBound, 'RECTANGLE_BY_WIDTH','#','#','MBG_FIELDS')
+
+        '''
+        ' Extract the minimum bounding rectangle orienatation angle to a variable
+        '''
+        for row in arcpy.da.SearchCursor(minBound,["MBG_Orientation","MBG_LENGTH","MBG_WIDTH"]):
+            orientation = row[0]
+            arcpy.AddMessage("Orientation Angle: {0}".format(str(orientation)))
+            if(orientation >= 45 and orientation <= 135):
+                horizontalCells = math.ceil(row[1]/float(cellWidth))
+                verticalCells = math.ceil(row[2]/float(cellHeight))
+            else:
+                verticalCells = math.ceil(row[1]/float(cellWidth))
+                horizontalCells = math.ceil(row[2]/float(cellHeight))
+            arcpy.AddMessage('Creating Grid {0} x {1}'.format(horizontalCells,verticalCells))
+
+        arcpy.AddMessage(labelStartPos)        
+        '''
+        ' Set up labeling depending on start position
+        '''
+        if labelStartPos == "Upper-Left":
+            letterIndex = verticalCells - 1
+            secondLetterIndex = -1
+            if labelStyle != 'Numeric':
+                labelNumber = 0
+            else:
+                labelNumber = (verticalCells - 1) * horizontalCells
+        elif labelStartPos == "Upper-Right":            
+            letterIndex = verticalCells - 1
+            secondLetterIndex = horizontalCells
+            if labelStyle != 'Numeric':
+                labelNumber = horizontalCells + 1
+            else:
+                labelNumber = (verticalCells * horizontalCells) + 1
+        elif labelStartPos == "Lower-Right":
+            letterIndex = 0
+            secondLetterIndex = horizontalCells
+            labelNumber = horizontalCells + 1
+        elif labelStartPos == "Lower-Left":
+            letterIndex = 0
+            secondLetterIndex = -1      
+            labelNumber = 0
+            
+        '''
+        ' Explode the minimum bounding rectangle to points
+        '''
+        with arcpy.da.SearchCursor(minBound, 'SHAPE@XY', explode_to_points=True) as cursor:
+            pts = [r[0] for r in cursor][0:4]
+
+        '''
+        ' Because the fishnet tool always creates a non rotated fishnet before it applies
+        ' its rotation we need to determine what the opposite point location would be if the
+        ' minimum polygon was not rotated
+        '''
+        if orientation < 45:
+            angle = math.radians(orientation)
+            x = float(pts[2][0]) - float(pts[0][0])
+            y = float(pts[2][1]) - float(pts[0][1])
+            xr = (x * math.cos(angle)) - (y * math.sin(angle)) + float(pts[0][0])
+            yr = (x * math.sin(angle)) + (y * math.cos(angle)) + float(pts[0][1])
+            origin = str(pts[0][0]) + ' ' + str(pts[0][1])
+            yaxis =  str(pts[1][0]) + ' ' + str(pts[1][1])
+        elif orientation >= 45 and orientation <= 135:
+            angle = math.radians((90 - orientation) * -1)
+            x = float(pts[1][0]) - float(pts[3][0])
+            y = float(pts[1][1]) - float(pts[3][1])
+            xr = (x * math.cos(angle)) - (y * math.sin(angle)) + float(pts[3][0])
+            yr = (x * math.sin(angle)) + (y * math.cos(angle)) + float(pts[3][1])
+            origin = str(pts[3][0]) + ' ' + str(pts[3][1])
+            yaxis =  str(pts[0][0]) + ' ' + str(pts[0][1])
+        else:
+            angle = math.radians((180 - orientation) * -1)            
+            x = float(pts[0][0]) - float(pts[2][0])
+            y = float(pts[0][1]) - float(pts[2][1])
+            xr = (x * math.cos(angle)) - (y * math.sin(angle)) + float(pts[2][0])
+            yr = (x * math.sin(angle)) + (y * math.cos(angle)) + float(pts[2][1])
+            origin = str(pts[2][0]) + ' ' + str(pts[2][1])
+            yaxis =  str(pts[3][0]) + ' ' + str(pts[3][1])
+        
+        oppositeCorner = str(xr) + " " + str(yr)
+        
+        '''
+        ' Now use the CreateFishnet_management tool to create the desired grid
+        '''                
+        arcpy.AddMessage("Creating Fishnet Grid...")
+        arcpy.CreateFishnet_management(fishnet, origin, yaxis, str(cellWidth), str(cellHeight), 0, 0, oppositeCorner, "NO_LABELS", fc, "POLYGON")
+        
+        '''
+        ' Add a field which will be used to add the grid labels
+        '''
+        arcpy.AddMessage("Adding field for labeling the grid")
+        gridField = "Grid"
+        arcpy.AddField_management(fishnet, gridField, "TEXT")
+
+        '''
+        ' Loop through features and label
+        '''
+        with arcpy.da.UpdateCursor(fishnet, ['OID','Grid']) as cursor:
+            verticalCount = 0
+            horizontalCount = 0
+            for row in cursor:
+                if labelStartPos == "Lower-Left" or labelStartPos == 'Upper-Left':
+                    labelNumber = labelNumber + 1
+                    secondLetterIndex = secondLetterIndex + 1                    
+                else:
+                    labelNumber = labelNumber - 1
+                    secondLetterIndex = secondLetterIndex - 1
+                
+                letter = ColIdxToXlName(int(letterIndex))
+                secondLetter = ColIdxToXlName(int(secondLetterIndex))    
+                
+                if (labelStyle == "Alpha-Numeric"):
+                    row[1] = letter + str(int(labelNumber))
+                elif (labelStyle == "Alpha-Alpha"):
+                    row[1] = letter + secondLetter
+                elif (labelStyle == "Numeric"):
+                    row[1] = labelNumber              
+                
+                cursor.updateRow(row)
+                
+                horizontalCount = horizontalCount + 1
+                
+                if horizontalCount >= horizontalCells:
+                    horizontalCount = 0
+                    verticalCount = verticalCount + 1
+                    if labelStartPos == "Upper-Left":
+                        letterIndex = letterIndex - 1
+                        secondLetterIndex = -1
+                        if labelStyle != 'Numeric':
+                            labelNumber = 0
+                        else:
+                            labelNumber = (verticalCells - (verticalCount + 1)) * horizontalCells
+                    elif labelStartPos == "Upper-Right":            
+                        letterIndex = letterIndex - 1
+                        secondLetterIndex = horizontalCells
+                        if labelStyle != 'Numeric':
+                            labelNumber = horizontalCells + 1                    
+                    elif labelStartPos == "Lower-Right":
+                        letterIndex = letterIndex + 1
+                        secondLetterIndex = horizontalCells
+                        if labelStyle != 'Numeric':
+                            labelNumber = horizontalCells + 1
+                        else:
+                            labelNumber = ((verticalCount + 1) * horizontalCells) + 1
+                    elif labelStartPos == "Lower-Left":
+                        letterIndex = letterIndex + 1
+                        secondLetterIndex = -1      
+                        if labelStyle != 'Numeric':
+                            labelNumber = 0
+                            
+        '''
+        ' Copy features to output feature class
+        '''
+        arcpy.CopyFeatures_management(fishnet, outputFeatureClass)
+        
+        '''
+        ' remove the temp feature classes
+        '''        
+        arcpy.Delete_management(fishnet)
+        
+        # Get and label the output feature
+        #TODO: Update once applying symbology in Pro is fixed.
+        #UPDATE
+        targetLayerName = os.path.basename(outputFeatureClass)
+        if appEnvironment == "ARCGIS_PRO":
+            #params = arcpy.GetParameterInfo()
+            ## get the symbology from the GRG.lyr
+            ##scriptPath = sys.path[0]
+            # layerFilePath = os.path.join(scriptPath,r"commondata\userdata\GRG.lyrx")
+            #arcpy.AddMessage("Applying Symbology from {0}".format(layerFilePath))
+            #params[6].symbology = layerFilePath
+            
+            arcpy.AddMessage("Do not apply symbology it will be done in the next task step")
+            
+        elif appEnvironment == "ARCMAP":
+                           
+            #arcpy.AddMessage("Adding features to map (" + str(targetLayerName) + ")...")
+            
+            #arcpy.MakeFeatureLayer_management(outputFeatureClass, targetLayerName)
+            
+            # create a layer object
+            #layer = arcpy.mapping.Layer(targetLayerName)            
+            
+            # get the symbology from the NumberedStructures.lyr
+            #layerFilePath = os.path.join(os.getcwd(),"data\Layers\GRG.lyr")
+            #layerFilePath = os.path.join(os.path.dirname(os.path.dirname(__file__)),"layers\GRG.lyr")
+            
+            # apply the symbology to the layer
+            #arcpy.ApplySymbologyFromLayer_management(layer, layerFilePath)
+            
+            # add layer to map
+            #arcpy.mapping.AddLayer(df, layer, "AUTO_ARRANGE")
+            
+            # find the target layer in the map
+            #mapLyr = arcpy.mapping.ListLayers(mxd, targetLayerName)[0]  
+
+            #arcpy.AddMessage("Labeling output features (" + str(targetLayerName) + ")...")
+            # Work around needed as ApplySymbologyFromLayer_management does not honour labels
+            #labelLyr = arcpy.mapping.Layer(layerFilePath)
+            # copy the label info from the source to the map layer
+            #mapLyr.labelClasses = labelLyr.labelClasses
+            # turn labels on
+            #mapLyr.showLabels = True
+            arcpy.AddMessage("Non-map environment, skipping labeling based on best practices")
+        else:
+            arcpy.AddMessage("Non-map environment, skipping labeling...")
+
+        # Set tool output
+        return outputFeatureClass
+
+        ### Apply symbology to the GRG layer
+        ###UPDATE
+        ###symbologyPath = os.path.dirname(workspace) + "\\Layers\GRG.lyr"
+        ###arcpy.ApplySymbologyFromLayer_management(layer, symbologyPath)
+
+    except arcpy.ExecuteError: 
+        # Get the tool error messages
+        msgs = arcpy.GetMessages()
+        arcpy.AddError(msgs)
+        print(msgs)
+
+    except:
+        # Get the traceback object
+        tb = sys.exc_info()[2]
+        tbinfo = traceback.format_tb(tb)[0]
+
+        # Concatenate information together concerning the error into a message string
+        pymsg = "PYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(sys.exc_info()[1])
+        msgs = "ArcPy ERRORS:\n" + arcpy.GetMessages() + "\n"
+
+        # Return python error messages for use in script tool or Python Window
+        arcpy.AddError(pymsg)
+        arcpy.AddError(msgs)
+    
+        # Print Python error messages for use in Python / Python Window
+        print(pymsg + "\n")
+        print(msgs)
