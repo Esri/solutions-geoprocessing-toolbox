dd6987ddf92222abe89e666748ab57808e197383
Chris Moore <csmooreacm@acm.org>
2017-09-25 11:42:16 -0400

    Update tests to check tool returned value and add test case for #607
    Changes:
    1. Update NumberFeatures tests to check tool returned value and add test
    case for #607
    2. Also noticed that testClearingOperationsNumberFeatures_NoField case
    was not actually testing this case correctly (was using a feature class
    that already had that field) so fixed
    3. Other minor improvements/commenting

--- utils/test/clearing_operations_tests/ClearingOperationsNumberFeaturesTestCase.py
+++ utils/test/clearing_operations_tests/ClearingOperationsNumberFeaturesTestCase.py
@@ -37,9 +37,8 @@ class ClearingOperationsNumberFeaturesTestCase(unittest.TestCase):
     inputArea = None
     pointFeatures = None
     output = None
-
-
     scratchGDB = None
+
     def setUp(self):
         if Configuration.DEBUG == True: print("         ClearingOperationsNumberFeaturesTestCase.setUp")
 
@@ -60,7 +59,6 @@ class ClearingOperationsNumberFeaturesTestCase(unittest.TestCase):
         self.inputArea = os.path.join(Configuration.clearingOperationsInputGDB, r"AO")
         self.pointFeatures = os.path.join(Configuration.clearingOperationsInputGDB, r"Structures")
 
-
         UnitTestUtilities.checkFilePaths([Configuration.clearingOperationsPath])
 
         UnitTestUtilities.checkGeoObjects([Configuration.clearingOperationsInputGDB, self.toolboxUnderTest, self.scratchGDB, self.inputArea, self.pointFeatures])
@@ -78,65 +76,172 @@ class ClearingOperationsNumberFeaturesTestCase(unittest.TestCase):
         #inputs
         fieldToNumber = "Number"
         output = os.path.join(self.scratchGDB, "numFields")
-        #Testing
+
+        # Start Test
         runToolMsg="Running tool (Number Features)"
         arcpy.AddMessage(runToolMsg)
         Configuration.Logger.info(runToolMsg)
 
+        toolOutput = None
+
         try:
-        #Calling the NumberFeatures_ClearingOperations Script Tool
-            arcpy.NumberFeatures_clrops(self.inputArea, self.pointFeatures, fieldToNumber, output)
+            #Calling the NumberFeatures_ClearingOperations Script Tool
+            toolOutput = arcpy.NumberFeatures_clrops(self.inputArea, self.pointFeatures, fieldToNumber, output)
         except arcpy.ExecuteError:
             UnitTestUtilities.handleArcPyError()
         except:
             UnitTestUtilities.handleGeneralError()
 
+        # 1: Check the expected return value (output feature class)
+        if toolOutput is None:
+            returnedValue = "BAD"
+        else:
+            returnedValue = toolOutput.getOutput(0)
+        self.assertEqual(returnedValue, output)  
+
+        # 2: Check the number of records
         result = arcpy.GetCount_management(output)
         count = int(result.getOutput(0))
-
-        cursor = arcpy.SearchCursor(output)
-        row = cursor.next()
-        val = row.getValue(fieldToNumber)
-        print("Field number first row: " + str(val) + " should not be null")
-
-        self.assertIsNotNone(val)
-
-        print("number features: " + str(count))
+        # print("number features: " + str(count))
+        # Important only 90 of 120 records are in the area of interest
         self.assertEqual(count, 90)
 
+        # 3: Check that the "number" field was created/updated for each row
+        cursor = arcpy.SearchCursor(output)
+        row = cursor.next()
+        while row is not None:
+            val = row.getValue(fieldToNumber)
+            # Debug: print("Field number first row: " + str(val) + " should not be null")
+            self.assertIsNotNone(val)
+            row = cursor.next()
+
     def testClearingOperationsNumberFeatures_NoField(self):
-        if Configuration.DEBUG == True:print(".....ClearingOperationsNumberFeaturesTestCase.testClearingOperationsNumberFeatures")
+        if Configuration.DEBUG == True:print(".....ClearingOperationsNumberFeaturesTestCase.testClearingOperationsNumberFeatures_NoField")
         print("Importing toolbox...")
         arcpy.ImportToolbox(self.toolboxUnderTest)
         arcpy.env.overwriteOutput = True
 
+        ########################################################
+        # Test steps:
+        # 1. Copy a known feature class in the AOI to the scratch GDB
+        # 2. Delete the "Number" field
+        # 3. Run the NumberFeatures tool
+        # 4. Verify the output
+        ########################################################
+
         #inputs
+        inputPoints = os.path.join(self.scratchGDB, 'numFieldsInput_NoField')
+        inputFieldToNumber = 'Number'
+
+        # Copy the features of the original input FC into the scratch GDB
+        arcpy.CopyFeatures_management(self.pointFeatures, inputPoints)   
+
+        # Delete the "fieldToNumber" field (so we can see if it gets set later)
+        arcpy.DeleteField_management(inputPoints, inputFieldToNumber)
+
         fieldToNumber = None
-        output = os.path.join(self.scratchGDB, "numFields")
-        #Testing
+        output = os.path.join(self.scratchGDB, "numFieldsOutput_NoField")
+
+        # Start Test
         runToolMsg="Running tool (Number Features)"
         arcpy.AddMessage(runToolMsg)
         Configuration.Logger.info(runToolMsg)
 
+        toolOutput = None
+
         try:
-        #Calling the NumberFeatures_ClearingOperations Script Tool
-            arcpy.NumberFeatures_clrops(self.inputArea, self.pointFeatures, fieldToNumber, output)
+            #Calling the NumberFeatures_ClearingOperations Script Tool
+            toolOutput = arcpy.NumberFeatures_clrops(self.inputArea, inputPoints, \
+                fieldToNumber, output)
         except arcpy.ExecuteError:
             UnitTestUtilities.handleArcPyError()
         except:
             UnitTestUtilities.handleGeneralError()
 
+        # 1: Check the expected return value (output feature class)
+        if toolOutput is None:
+            returnedValue = "BAD"
+        else:
+            returnedValue = toolOutput.getOutput(0)
+        self.assertEqual(returnedValue, output)  
+
+        # 2 Check the number of records
         result = arcpy.GetCount_management(output)
         count = int(result.getOutput(0))
+        print("Number features: " + str(count))
+        # Important only 90 of 120 records are in the area of interest
+        self.assertEqual(count, 90)
 
+        # 3: Check that the "number" field was created/updated for each row
         cursor = arcpy.SearchCursor(output)
         row = cursor.next()
-        val = row.getValue("Number") #default field will still be 'Number'
-        print("Field number first row: " + str(val) + " should not be null")
-        self.assertIsNotNone(val)
+        while row is not None:
+            val = row.getValue("Number") #default field will still be 'Number'
+            # Debug: print("Field number first row: " + str(val) + " should not be null")
+            self.assertIsNotNone(val)
+            row = cursor.next()
 
-        print("number features: " + str(count))
-        self.assertEqual(count, 90)
+    # Added to test for: https://github.com/Esri/solutions-geoprocessing-toolbox/issues/607
+    def testClearingOperationsNumberFeatures_NoOutputParam(self):
+        if Configuration.DEBUG == True:print(".....ClearingOperationsNumberFeaturesTestCase.testClearingOperationsNumberFeatures_NoOutputParam")
+        print("Importing toolbox...")
+        arcpy.ImportToolbox(self.toolboxUnderTest)
+        arcpy.env.overwriteOutput = True
+
+        ########################################################
+        # Test steps:
+        # 1. Copy a known feature class in the AOI to the scratch GDB
+        # 2. Add a new "Number" field (so will be blank)
+        # 3. Run the NumberFeatures tool
+        # 4. Verify the output
+        ########################################################
+
+        #inputs
+        fieldToNumber = "NumberNewField"
+
+        inputPoints = os.path.join(self.scratchGDB, 'numFieldsInput_NoOutput')
+
+        # Copy the features of the original input FC into the scratch GDB
+        arcpy.CopyFeatures_management(self.pointFeatures, inputPoints)   
+
+        arcpy.AddField_management(inputPoints, fieldToNumber, "LONG")   
+
+        # Start Test
+        runToolMsg="Running tool (Number Features)"
+        arcpy.AddMessage(runToolMsg)
+        Configuration.Logger.info(runToolMsg)
+
+        toolOutput = None
+
+        try:
+            #Calling the NumberFeatures_ClearingOperations Script Tool
+            #No output parameter 
+            toolOutput = arcpy.NumberFeatures_clrops(self.inputArea, inputPoints, \
+                fieldToNumber)
+        except arcpy.ExecuteError:
+            UnitTestUtilities.handleArcPyError()
+        except:
+            UnitTestUtilities.handleGeneralError()
+
+        # 1: Check the expected return value (output feature class)
+        # TODO: this return value may change in the future, and this check may need updated
+        expectedOutput = ""
+        if toolOutput is None:
+            returnedValue = "BAD"
+        else:
+            returnedValue = toolOutput.getOutput(0)
+        self.assertEqual(returnedValue, expectedOutput)  
+
+        #2: Check the Output Feature class has no rows without a null Number
+        outputLayer = "Number_NoOutput"       
+        arcpy.MakeFeatureLayer_management(inputPoints, outputLayer)
+        query = '(' + fieldToNumber + ' is NOT NULL)'
+        
+        arcpy.SelectLayerByAttribute_management(outputLayer, "NEW_SELECTION", query)
+        recordCount = int(arcpy.GetCount_management(outputLayer).getOutput(0))
+
+        # Important only 90 of 120 records are in the area of interest
+        self.assertEqual(recordCount, int(90))
 
 if __name__ == "__main__":
     unittest.main()       
